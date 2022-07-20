cf2ca49cffc33af7f5287fb700f0a5b51e104701
Faustino Marco Simpliciano <marco.simpliciano@gmail.com>
2022-07-19 18:46:32 -0700

    workout out tests and sum function -- trying to get better at more frequent commit's :/ still forgetting a lot

--- series.py
+++ series.py
@@ -1,14 +1,12 @@
 print("Hello, World!")
 
+
 def fibonacci(n):
     if n == 0 or n == 1:
         return 1
     else:
         return fibonacci(n-1) + fibonacci(n-2)
 
-firstTry = fibonacci(9)
-
-print(firstTry)
 
 def lucas(n):
     if n == 0:
@@ -17,8 +15,31 @@ def lucas(n):
         return 1
     else:
         return lucas(n-1) + lucas(n-2)
+
+
 tryLucas = lucas(8)
 
 print(tryLucas)
 
-def sum_series(n, option1=0, option2=1):
+
+def sum_series(n, a=1, b=1):
+    """
+    Hi Kassie!!!!!!
+    """
+    # print(a)
+    # print(b)
+    if n == 0:
+        return a
+    if n == 1:
+        return b
+    else:
+        # print(b)
+        # print(n)
+        return sum_series(n-1, a, b) + sum_series(n-2, a, b)
+
+
+print(fibonacci(8))
+
+
+print(sum_series(2, 3, 4))
+
